d059fae43ab646df43db0c688bd4c14317ef7b64
Taylor Petrychyn <tpetrychyn@gmail.com>
2018-10-02 17:02:51 -0600

    app init

---
+++ app/app/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace app.Pages.Auth
+{
+    public class LoginModel : PageModel
+    {
+        public string Email { get; set; }
+        public void OnGet()
+        {
+        }
+
+        //public IAsyncResult OnPostAsync() {
+
+        //    return null;
+        //}
+    }
+}

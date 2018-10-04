7258617b08f20c98411d96a9c898230f6416ccca
Paul Hewitt <thephewitt@gmail.com>
2018-10-04 12:17:22 -0600

    Adding Registration Controller, Model, and Page

---
+++ eideas/eideas/Controllers/RegistrationController.cs
@@ -0,0 +1,15 @@
+﻿using eideas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eideas.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View("~/Views/Auth/Register.cshtml", new LoginModel());
+        }
+    }
+}
---
+++ eideas/eideas/Models/RegistrationModel.cs
@@ -0,0 +1,7 @@
+﻿namespace eideas.Models
+{
+    public class RegistrationModel
+    {
+        public string Email { get; set; }
+    }
+}

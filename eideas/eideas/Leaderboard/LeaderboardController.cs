8fea42b60f7444e11b04579a2f4af75e4693a127
NomadAsylum <NomadAsylum@users.noreply.github.com>
2018-12-03 23:16:18 -0600

    Leaderboards

--- eideas/eideas/Leaderboard/LeaderboardController.cs
+++ eideas/eideas/Leaderboard/LeaderboardController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using eideas.Models;
 
 namespace eideas.NewFolder
 {
@@ -24,29 +25,94 @@ namespace eideas.NewFolder
         [Authorize]
         public IActionResult Index(string filter)
         {
+            ICollection<EIdeasUser> localUsers = db.Users.Include(i => i.UserDivision).ToList();
+            ICollection<Idea> localIdeas = db.Ideas.Include(i => i.IdeaUpdoots).ToList();
+            ICollection<IdeaComment> localComments = db.IdeaComments.Include(i => i.CommentUpDoots).Include(i => i.EIdeasUser).ToList();
+            ICollection<Division> localDivisions = db.Divisions.Include(i => i.EideasUsers).ToList();
+            ICollection<Unit> localUnits = db.Units.Include(i => i.EideasUsers).ToList();
+            List<LeaderboardModel> list = new List<LeaderboardModel>();
 
-            ICollection<Idea> ideas = db.Ideas.Include(i => i.IdeaUpdoots).ToList();
-
-            if (filter != null)
-            {
+            
                 switch (filter)
                 {
-                    case "TopUser":
-                        ideas = ideas.OrderByDescending(a => a.IdeaUpdoots.Count()).ToList();
-                        break;
+                    
                     case "TopDivision":
-                        ideas = ideas.OrderByDescending(a => a.CreatedDate).ToList();
+                        foreach (Division division in localDivisions)
+                        {
+                            int divPoints = 0;
+                            foreach (EIdeasUser newName in localUsers)
+                            {
+                                foreach (var idea in localIdeas)
+                                {
+                                   if (idea.CreatedBy == newName.UserName && newName.UserDivisionDivisionId == division.DivisionId)
+                                    {
+                                         divPoints += idea.IdeaUpdoots.Count();
+                                    }
+                                }
+                                foreach (var comment in localComments)
+                                {
+                                    if (comment.EIdeasUser.UserName == newName.UserName && newName.UserDivisionDivisionId == division.DivisionId)
+                                    {
+                                         divPoints += comment.CommentUpDoots.Count();
+                                    }
+                                }
+                            }
+                            list.Add(new LeaderboardModel(divPoints, division.DivisionName));
+                        }
                         break;
                     case "TopUnit":
-                        ideas = ideas.OrderByDescending(a => a.PDCA).ToList();
+                        foreach (Unit unit in localUnits)
+                        {
+                            int unitPoints = 0;
+                            foreach (EIdeasUser newName in localUsers)
+                            {
+                                foreach (var idea in localIdeas)
+                                {
+                                   if (idea.CreatedBy == newName.UserName && newName.UserUnitUnitId == unit.UnitId)
+                                    {
+                                         unitPoints += idea.IdeaUpdoots.Count();
+                                    }
+                                }
+                                foreach (var comment in localComments)
+                                {
+                                    if (comment.EIdeasUser.UserName == newName.UserName && newName.UserUnitUnitId == unit.UnitId)
+                                    {
+                                         unitPoints += comment.CommentUpDoots.Count();
+                                    }
+                                }
+                            }
+                            list.Add(new LeaderboardModel(unitPoints, unit.UnitName));
+                        }
                         break;
+                    case "TopUser":
                     default:
+                       foreach (EIdeasUser newName in localUsers)
+                        {
+                            int userPoints = 0;
+
+                            //Get Idea Points
+                            foreach (var idea in localIdeas)
+                            {
+                                 if (idea.CreatedBy == newName.UserName)
+                                {
+                                     userPoints += idea.IdeaUpdoots.Count();
+                                 }
+                            }
+                            foreach (var comment in localComments)
+                            {
+                                if (comment.EIdeasUser.UserName == newName.UserName)
+                                {
+                                     userPoints += comment.CommentUpDoots.Count();
+                                }
+                            }
+                            list.Add(new LeaderboardModel(userPoints, newName.NormalizedEmail));
+                        }
                         break;
                 }
-            }
 
-
-            return View("~/Leaderboard/Leaderboard.cshtml", ideas);
+            ICollection<LeaderboardModel> modelio = list;
+            modelio = modelio.OrderByDescending(a => a.points).ToList();
+            return View("~/Leaderboard/Leaderboard.cshtml", modelio);
         }
 
 
---
+++ eideas/eideas/Models/LeaderboardModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using eideas.Areas.Identity.Data;
+using eideas.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace eideas.Models
+{
+    public class LeaderboardModel
+    {
+        public int points { get; set; }
+        public string Name { get; set; }
+        public LeaderboardModel()
+        {
+        }
+        public LeaderboardModel(int p, string n)
+        {
+        	points = p;
+        	Name = n;
+        }
+    }
+}
